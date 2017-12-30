3421b4a2cee4f2d3e7b7b075c8709b5604c3763a
Paul Johnson <paul@snuberry-software.co.uk>
2017-12-30 23:54:46 +0000

    initial import

---
+++ entryspin/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace entryspin
+{
+    public class BasePage : ContentPage
+    {
+        public BasePage()
+        {
+            if (Device.RuntimePlatform == Device.iOS)
+                Padding = new Thickness(0, 20);
+            BackgroundColor = Color.White;
+        }
+    }
+}
+
+
---
+++ entryspin/SpinPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+
+namespace entryspin
+{
+    public class SpinPage : BasePage
+    {
+        public SpinPage()
+        {
+            var entryText = new Entry
+            {
+                WidthRequest = 200,
+                TextColor = Color.Black,
+                Placeholder = "Enter text",
+                PlaceholderColor = Color.Gray
+            };
+
+            var imgTriangle = new Image
+            {
+                WidthRequest = 32,
+                HeightRequest = 32,
+                Source = "triangle.png"
+            };
+
+            var spinSpin = new Picker
+            {
+                TextColor = Color.Black
+            };
+
+            var text = new List<string> { "Hello", "From", "Me" };
+            foreach (var t in text)
+                spinSpin.Items.Add(t);
+
+            var horizStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Children = { entryText, imgTriangle }
+            };
+
+            var stack = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                Padding = new Thickness(12),
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+                Children = { horizStack }
+            };
+
+            var imgTap = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1,
+                Command = new Command(() =>
+                {
+                    stack.Children.Add(spinSpin);
+                })
+            };
+            imgTriangle.GestureRecognizers.Add(imgTap);
+
+            spinSpin.SelectedIndexChanged += (sender, e) =>
+            {
+                entryText.Text = text[spinSpin.SelectedIndex];
+            };
+
+            Content = stack;
+        }
+    }
+}
+
+
